a1c8afcb0e9cf34979a977b36b4e2cc4fe5ca6af
jtnpals <jtnpals@gmail.com>
2020-07-31 00:54:35 +0900

    Add listuser.ejs

--- ViewExample/routes/user.js
+++ ViewExample/routes/user.js
@@ -1,3 +1,10 @@
+/*
+ * 사용자 정보 처리 모듈
+ * 데이터베이스 관련 객체들을 req.app.get('database')로 참조
+ *
+ * @date 2016-11-10
+ * @author Mike
+ */
 
 var login = function(req, res) {
 	console.log('user(user2.js) 모듈 안에 있는 login 호출됨.');
@@ -52,7 +59,7 @@ var login = function(req, res) {
 					
 					res.end(html);
 				});
-			
+				
 			} else {  // 조회된 레코드가 없는 경우 실패 응답 전송
 				res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
 				res.write('<h1>로그인  실패</h1>');
@@ -102,8 +109,26 @@ var adduser = function(req, res) {
 				console.dir(addedUser);
  
 				res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
-				res.write('<h2>사용자 추가 성공</h2>');
-				res.end();
+				
+				// 뷰 템플레이트를 이용하여 렌더링한 후 전송
+				var context = {title:'사용자 추가 성공'};
+				req.app.render('adduser', context, function(err, html) {
+					if (err) {
+                        console.error('뷰 렌더링 중 에러 발생 : ' + err.stack);
+                
+                        res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+				        res.write('<h2>뷰 렌더링 중 에러 발생</h2>');
+                        res.write('<p>' + err.stack + '</p>');
+				        res.end();
+                
+                        return;
+                    }
+					
+					console.log("rendered : " + html);
+					
+					res.end(html);
+				});
+				
 			} else {  // 결과 객체가 없으면 실패 응답 전송
 				res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
 				res.write('<h2>사용자 추가  실패</h2>');
@@ -144,17 +169,25 @@ var listuser = function(req, res) {
 				console.dir(results);
  
 				res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
-				res.write('<h2>사용자 리스트</h2>');
-				res.write('<div><ul>');
 				
-				for (var i = 0; i < results.length; i++) {
-					var curId = results[i]._doc.id;
-					var curName = results[i]._doc.name;
-					res.write('    <li>#' + i + ' : ' + curId + ', ' + curName + '</li>');
-				}	
-			
-				res.write('</ul></div>');
-				res.end();
+				// 뷰 템플레이트를 이용하여 렌더링한 후 전송
+				var context = {results:results};
+				req.app.render('listuser', context, function(err, html) {
+				    if (err) {
+                        console.error('뷰 렌더링 중 에러 발생 : ' + err.stack);
+                
+                        res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+				        res.write('<h2>뷰 렌더링 중 에러 발생</h2>');
+                        res.write('<p>' + err.stack + '</p>');
+				        res.end();
+                
+                        return;
+                    }
+					console.log('rendered : ' + html);
+					
+					res.end(html);
+				});
+				
 			} else {
 				res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
 				res.write('<h2>사용자 리스트 조회  실패</h2>');
